b8e9dd4877be09e09265d3b6443a8d69df7f50a3
ethanjmli <ethan.jm.li@gmail.com>
2023-08-21 19:39:13 -0400

    Installing packages and testing Latex install

--- R/EPI563.R
+++ R/EPI563.R
@@ -1,2 +1,13 @@
 #EPI563 Code
 #Fall 2023
+# install.packages('tidyverse')   
+# install.packages(c('pacman', 'here'))
+# install.packages(c('tinytex', 'rmarkdown', 'knitr')) 
+# tinytex::install_tinytex() 
+# install.packages(c('tidycensus','tigris')) 
+# 
+# help('census_api_key','tidycensus')
+# install.packages(c('sp', 'sf', 'raster', 'RColorBrewer', 'rgeos', 'rgdal', 'maptools', 'OpenStreetMap'))  
+# install.packages(c('tmap', 'tmaptools')) 
+# install.packages(c('spdep', 'CARBayes', 'sparr', 'spatialreg',  'DCluster', 'SpatialEpi', 'smerc'))
+# install.packages(c('GWmodel', 'spgwr') )
